d34b77874213c8fb52c2d4b7475f440e6f9f3559
Daniel Shiffman <daniel@shiffman.net>
2018-06-26 17:55:23 -0400

    adding color classifier and renumbering

---
+++ 02_Color_Classifier/sketch.js
@@ -0,0 +1,47 @@
+let data;
+
+let labelList = [
+  'red-ish',
+  'green-ish',
+  'blue-ish',
+  'orange-ish',
+  'yellow-ish',
+  'pink-ish',
+  'purple-ish',
+  'brown-ish',
+  'grey-ish'
+]
+
+function preload() {
+  data = loadJSON('colorData.json');
+}
+
+
+function setup() {
+  //console.log(data.entries.length);
+
+  let colors = [];
+  let labels = [];
+  for (let record of data.entries) {
+    let col = [record.r / 255, record.g / 255, record.b / 255];
+    colors.push(col);
+    labels.push(labelList.indexOf(record.label));
+  }
+  //console.log(colors);
+
+  let xs = tf.tensor2d(colors);
+  //console.log(xs.shape);
+  //xs.print();
+
+  let labelsTensor = tf.tensor1d(labels, 'int32');
+  labelsTensor.print();
+
+  let ys = tf.oneHot(labelsTensor, 9);
+  labelsTensor.dispose();
+
+
+
+
+  //console.log(labels);
+
+}
